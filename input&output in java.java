94109245f8fa757de53904c31a3095046b05f515
Yash Thakur <117333139+yashthakur16@users.noreply.github.com>
2023-10-16 23:49:50 +0530

    Create input&output in java.java

---
+++ input&output in java.java
@@ -0,0 +1,15 @@
+package CLGAssignment;
+
+public class Ram 
+{
+
+    String name="Ram";
+    int salary = 50000;
+    
+    public static void main(String [] args)
+    {
+        Ram R = new Ram();
+        System.out.println(R.name);
+        System.out.println(R.salary);
+    }
+}
